3b1a806e174e3f65b81d927df8f552ddda90ff6c
Sam Miller <sam.miller74@hotmail.com>
2018-02-05 21:25:42 +0000

    Added get, add and remove criteria methods to the waiterService

--- src/services/waiter.service.ts
+++ src/services/waiter.service.ts
@@ -2,6 +2,7 @@ import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
 import {DbService} from "./db.service";
 import {Waiter} from "../types/waiter";
+import { Criteria } from "../types/criteria";
 
 @Injectable()
 export class WaiterService {
@@ -27,6 +28,7 @@ export class WaiterService {
               for(let i = 0; i < data.rows.length; i++) {
                 temp = { firstName: data.rows.item(i).FirstName
                   , lastName: data.rows.item(i).LastName
+                  , criteria: this.getCriteria(data.rows.item(i).WaiterID)
                   , waiterId: data.rows.item(i).WaiterID };
                 waiters.push(temp);
               }
@@ -92,9 +94,7 @@ export class WaiterService {
                                  WHERE WaiterID = ?;`,
           [waiter.firstName, waiter.lastName, waiter.waiterId] )
           .then(() => {
-            observer.next({name: waiter.lastName,
-              description: waiter.firstName,
-              points: waiter.waiterId});
+            observer.next(waiter);
             observer.complete();
           }).catch(error => {
           observer.error(new Error(error.message));
@@ -106,4 +106,71 @@ export class WaiterService {
       });
     });
   }
+
+  private getCriteria(waiterId: string) {
+    return Observable.create(observer => {
+      this.db.connectDb().subscribe(cn => {
+        cn.executeSql(`SELECT CRITERIA.CriteriaID
+          , CRITERIA.Name
+          , CRITERIA.Description
+          , CRITERIA.Points
+          FROM CRITERIA
+            LEFT JOIN WAITER_CRITERIA ON CRITERIA.CriteriaID = WAITER_CRITERIA.CriteriaID
+          WHERE WAITER_CRITERIA.WaiterID = ?`, [waiterId])
+          .then(data => {
+            let criteria:Array<Criteria> = [];
+            if(data.rows.length > 0) {
+              let temp;
+              for(let i = 0; i < data.rows.length; i++) {
+                temp = { name: data.rows.item(i).Name
+                  , description: data.rows.item(i).Description
+                  , points: data.rows.item(i).Points
+                  , criteriaId: data.rows.item(i).CriteriaID };
+                criteria.push(temp);
+              }
+            }
+          })
+      });
+    });
+  }
+
+  public addCriteria(criteria: Criteria, waiter: Waiter) {
+    return Observable.create(observer => {
+      this.db.connectDb().subscribe(cn => {
+        cn.executeSql(`INSERT INTO WAITER_CRITERIA (WaiterID, CriteriaID) VALUES (?,?);`,
+           [criteria.criteriaId, waiter.waiterId])
+          .then(() => {
+            observer.next(criteria);
+            observer.complete();
+          }).catch(error => {
+          observer.error(new Error(error.message));
+          observer.complete();
+        });
+      }, connErr => {
+        observer.error(new Error(`Unable to connect to database ${connErr}`));
+        observer.complete();
+      });
+    });
+  }
+
+  public removeCriteria(criteria: Criteria, waiter: Waiter) {
+    return Observable.create(observer => {
+      this.db.connectDb().subscribe(cn => {
+        cn.executeSql(`DELETE FROM WAITER_CRITERIA 
+          WHERE WaiterID = ? AND CriteriaID = ?;`,
+           [criteria.criteriaId, waiter.waiterId])
+          .then(() => {
+            observer.next(criteria);
+            observer.complete();
+          }).catch(error => {
+          observer.error(new Error(error.message));
+          observer.complete();
+        });
+      }, connErr => {
+        observer.error(new Error(`Unable to connect to database ${connErr}`));
+        observer.complete();
+      });
+    });
+  }
+
 }
